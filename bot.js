ba4cad607603b92523f2ef12083555acd7f2c8cc
opsJson <cleversonpelegrini@gmail.com>
2023-04-06 11:46:24 -0300

    all chats inside playground category is a playground

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const {Client, GatewayIntentBits, ThreadChannel} = require("discord.js");
+const {Client, GatewayIntentBits} = require("discord.js");
 const fetch = require("node-fetch");
 
 const client = new Client({intents:[
@@ -6,110 +6,57 @@ const client = new Client({intents:[
 	GatewayIntentBits.GuildMessages,
 	GatewayIntentBits.MessageContent
 ]});
-
 const DiscordToken = "discord-token";
-const OpenAIToken = "openai-token";
 const MaxChars = 8200;
-let ChatGPTChannelID;
+
+let OpenAIToken;
+let ChatGPTParentID;
 
 client.on("ready", () => {
 	client.application.commands.create({
-		name: "channel",
-		description: "Seta o playground",
-		options: [{
+		name: "config",
+		description: "Sets OpenAI token and playground category ID",
+		options: [
+		{
+			name: "token",
+			description: "OpenAI token",
+			type: 3
+		},
+		{
 			name: "id",
-			description: "Channel ID",
-			type: 3,
-			required: true
+			description: "Playground category ID",
+			type: 3			
 		}]
-	});
-	client.application.commands.create({
-		name: "closeall",
-		description: "Fecha todas as threads"
 	});
 	console.log("ChatGPT Bot online!");
 });
 
 client.on("interactionCreate", async (interaction) => {
-	if (interaction.commandName == "channel") {
-		const channelID = interaction.options.getString("id").trim();
-		const channel = await client.channels.cache.get(channelID);
-		
-		if (channel) {
-			ChatGPTChannelID = channelID;
-			interaction.reply({
-				content: `Canal playground setado: <#${ChatGPTChannelID}>`,
-				ephemeral: true
-			});
-		}
-		else {
-			interaction.reply({
-				content: `Canal não encontrado: **${channelID}**`,
-				ephemeral: true
-			});
-		}
-	}
-	else if (interaction.commandName == "closeall") {
-		if (!ChatGPTChannelID) {
-			interaction.reply({
-				content: "Canal playground ainda não foi setado!",
-				ephemeral: true
-			});
-		}
-		if (interaction.channel.id != ChatGPTChannelID) {
-			interaction.reply({
-				content: `Você só pode usar esse comando aqui: <#${ChatGPTChannelID}>`,
-				ephemeral: true
-			});
-			return;
-		}
-		
-		interaction.reply({
-			content: "Deletando todas as threads...",
-			ephemeral: true
-		});
-		
-		const fetched = await interaction.channel.threads.fetch();
-		fetched.threads.forEach(thread => {
-			if (thread.parentId != ChatGPTChannelID) return;
-			try {
-				thread.delete();
-			}
-			catch (e) {
-				console.error(e);
-			}
-		});
-	}
+	OpenAIToken = interaction.options.getString("token")?.trim() || OpenAIToken;
+	ChatGPTParentID = interaction.options.getString("id")?.trim() || ChatGPTParentID;
+	
+	interaction.reply({
+		content: "The new settings have been saved.",
+		ephemeral: true
+	});
 });
 
 client.on("messageCreate", async (message) => {
-	if (!ChatGPTChannelID) return;
 	if (message.author.bot) return;
+	if (!message.channel.parent) return;
 	if (message.content.startsWith("!")) return;
-	if (message.channel.parentId != ChatGPTChannelID
-	 && message.channel.id != ChatGPTChannelID) return;
+	if (message.channel.parent != ChatGPTParentID) return;
 	
-	let thread;
-	
-	if (message.channel instanceof ThreadChannel) {
-		thread = message.channel;
-	}
-	else {
-		thread = await message.startThread({
-			name: message.content.slice(0, 22)
-		});
-	}
-	
-	const messages = await thread.messages.fetch();
+	const messages = await message.channel.messages.fetch();
 	const chat = [];
 	let counter = 0;
 	
 	messages.forEach(message => {
 		counter += message.content.length;
 
-		if (counter > MaxChars) return false;
-		if (message.content.length == 0) return false;
-		if (message.content.startsWith("!")) return false;
+		if (counter > MaxChars) return;
+		if (message.content.length == 0) return;
+		if (message.content.startsWith("!")) return;
 		
 		chat.push({
 			role: message.author.id == client.user.id ? "assistant" : "user",
@@ -117,17 +64,11 @@ client.on("messageCreate", async (message) => {
 		});
 	});
 	
-	const StarterMessage = await thread.fetchStarterMessage();
-	chat.push({
-		role: "user",
-		content: StarterMessage.content
-	});
-	
 	if (chat.length > 1) chat.reverse();
 	
-	const interval = setInterval(() => thread.sendTyping(), 5000);
-	thread.sendTyping();
-	console.log(chat);
+	const interval = setInterval(() => message.channel.sendTyping(), 5000);
+	message.channel.sendTyping();
+	
 	try {
 		const response = await fetch("https://api.openai.com/v1/chat/completions", {
 			method: "POST",
@@ -142,8 +83,7 @@ client.on("messageCreate", async (message) => {
 		});
 		
 		if (response.status != 200) {
-			thread.send("Erro interno: API retornou status code diferente de 200.");
-			return;
+			throw "Erro interno: API retornou status code diferente de 200.";
 		}
 		
 		let r = await response.json();
@@ -152,15 +92,13 @@ client.on("messageCreate", async (message) => {
 		
 		clearInterval(interval);
 		r.forEach(chunk => {
-			thread.send(chunk);
+			message.channel.send(chunk);
 		});
 	}
 	catch(e) {
-		thread.send("Erro interno: API lançou alguma excessão.");
-		console.log(e);
+		clearInterval(interval); 
+		message.channel.send("Erro interno!");
 	}
-	
-	clearInterval(interval);
 });
 
 client.login(DiscordToken);
